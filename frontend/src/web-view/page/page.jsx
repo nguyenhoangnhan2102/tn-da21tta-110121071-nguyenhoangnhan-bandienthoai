b67a22fac7d0d839d2e30bfa3c1a293dea488079
Nguyễn Hoàng Nhân <nhnhan2102@gmail.com>
2025-05-15 22:38:50 +0700

    update

--- frontend/src/web-view/page/page.jsx
+++ frontend/src/web-view/page/page.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const MainPage = () => {
   return (
     <div>
-      <h1>Welcome to the Main Page</h1>
-      <p>This is the main content of the page.</p>
+      <h1>
+        <a href="/login">Login</a>
+        <a href="/admin">Admin</a>
+      </h1>
     </div>
   );
 };
