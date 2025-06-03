bd9b5dd9b161afcad18dc4b79d63bc39bb11e619
Nguyễn Hoàng Nhân <nhnhan2102@gmail.com>
2025-06-03 16:27:05 +0700

    update

--- frontend/src/user-view/router-user.jsx
+++ frontend/src/user-view/router-user.jsx
@@ -1,18 +1,14 @@
 import { useRoutes, Navigate } from "react-router-dom";
+
 import Profile from "./profile/page";
-import UserLayout from "./profile/layout";
 
 const UserRouter = () => {
   const element = useRoutes([
     {
-      path: "/profile",
-      element: <UserLayout />,
-      children: [
-        { path: "", element: <Navigate to="info" replace /> }, // default tab
-        { path: "info", element: <Profile /> },
-        // { path: "orders", element: <OrdersTab /> },
-      ],
+      path: "/",
+      element: <Profile />,
     },
+
     {
       path: "*",
       element: <Navigate to="/login" replace />,
